e3e92674dc7586a9334cd7b178e3b6c607b35fae
tobiascrackz <63141918+tobiascrackz@users.noreply.github.com>
2026-03-14 09:51:10 +0100

    add inaInit declaration

--- src/Power.h
+++ src/Power.h
@@ -125,6 +125,7 @@ class Power : public concurrency::OSThread
     void lipoChargerRetry();
     /// Setup a meshSolar battery sensor
     bool meshSolarInit();
+    bool inaInit();
     /// Setup a serial battery sensor
     bool serialBatteryInit();
 #ifdef HAS_ADS1115
