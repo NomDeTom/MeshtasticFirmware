576fe09720dac00f713ce9b77567b65231059f27
nixvio64 <167223800+nixvio64@users.noreply.github.com>
2026-05-21 21:08:59 +0200

    My Variants

---
+++ variants/esp32s3/n16r8/pins_arduino.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <stdint.h>
+
+static const uint8_t SDA = 21;
+static const uint8_t SCL = 47;
+
+static const uint8_t SS   = 10;
+static const uint8_t MOSI = 11;
+static const uint8_t MISO = 4;
+static const uint8_t SCK  = 12;
+
+static const uint8_t A0  = 1;
+static const uint8_t LED_BUILTIN = 2;
---
+++ variants/esp32s3/n16r8_minimal/variant.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define HAS_SCREEN 0
+#define HAS_GPS    0
+
+#define I2C_SDA    21
+#define I2C_SCL    20
+
+#define BUTTON_PIN 0
+
+#define USE_SX1262
+#define SX126X_CS                15
+#define SX126X_DIO1              16
+#define SX126X_BUSY              17
+#define SX126X_RESET             18
+#define SX126X_RXEN              8
+#define SX126X_TXEN              7
